537b67a30092be865fd0881c4016f7bc774a6fb7
galaxyman7 <colbylippincott@gmail.com>
2025-12-18 17:16:50 -0800

    Delete js directory

--- js/storage.js
+++
@@ -1,9 +0,0 @@
-/**
- * Save and load entire JSON session. Hidden info remains hidden by UI filtering.
- */
-export class Storage {
-  constructor(bus, state) {
-    this.bus = bus;
-    this.state = state;
-  }
-}
